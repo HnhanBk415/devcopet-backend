bbc0bf3d55dfc473f3c8d57585044df4ca96e30f
Võ Chí Thành <vochithanh@MacBook-Pro-cua-Vo.local>
2026-04-22 22:23:12 +0700

    feat: init project NestJS, setup MongoDB và Husky

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('users') // Định tuyến: endpoint lúc này sẽ là /users
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get() // Nhận Request GET /users
+  async getUsers() {
+    return this.appService.getAllUsers();
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User, UserSchema } from './user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://devcopet:bkute123456@cluster-1.7fhl9ez.mongodb.net/devcopet?retryWrites=true&w=majority&appName=Cluster-1',
+    ),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
