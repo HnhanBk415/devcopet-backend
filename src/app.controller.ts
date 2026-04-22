3152c997d748a3d0faa4d37c0b05fc3b8b79a530
Võ Chí Thành <vochithanh@MacBook-Pro-cua-Vo.local>
2026-04-22 21:13:37 +0700

    init: khoi tao project NestJS, ket noi MongoDB va setup Husky

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Nhận Request GET /users
   async getUsers() {
     return this.appService.getAllUsers();
   }
