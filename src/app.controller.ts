20e66acef23315eb1ef816246d7e0bff173feb30
Võ Chí Thành <vochithanh@MacBook-Pro-cua-Vo.local>
2026-04-22 21:50:20 +0700

    fix: sua duong dan thanh information

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('users') // Định tuyến: endpoint lúc này sẽ là /users
+@Controller('information') // Định tuyến: endpoint lúc này sẽ là /users
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
