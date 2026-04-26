61f9fea2c4382bcfb2ac1266905e94f09d97f589
Võ Chí Thành <vochithanh@MacBook-Pro-cua-Vo.local>
2026-04-26 14:02:15 +0700

    chore: setup .env and restructure modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User, UserSchema } from './user.schema';
+import { User, UserSchema } from './modules/users/schemas/user.schema';
+import { UsersModule } from './modules/users/users.module';
+import { TutorialsModule } from './modules/tutorials/tutorials.module';
+import { RoadmapsModule } from './modules/roadmaps/roadmaps.module';
+import { HistoriesModule } from './modules/histories/histories.module';
 
 @Module({
   imports: [
@@ -10,6 +14,10 @@ import { User, UserSchema } from './user.schema';
       'mongodb+srv://devcopet:bkute123456@cluster-1.7fhl9ez.mongodb.net/devcopet?retryWrites=true&w=majority&appName=Cluster-1',
     ),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
+    TutorialsModule,
+    RoadmapsModule,
+    HistoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
