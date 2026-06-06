3079be08930d90d7977f50ffe2343882f45391df
HnBk415 <hoang.nhanbkag@gmail.com>
2026-06-06 12:30:38 +0700

    update them

--- src/modules/chapters-lessons/chapters-lessons.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ChaptersLessonsService } from './chapters-lessons.service';
-
-@Controller('chapters-lessons')
-export class ChaptersLessonsController {
-  constructor(
-    private readonly chaptersLessonsService: ChaptersLessonsService,
-  ) {}
-}
